5b2996a47e9961935302a64a97ada3ac3e87fafc
Mark.Chen(Shao-Yuan Chen) <markchen0114@gmail.com>
2018-04-27 00:31:26 +0800

    [MySQL] Allow Remote Connection

--- MySQL.sql
+++ MySQL.sql
@@ -3,3 +3,7 @@ SET SQL_SAFE_UPDATES = 0;
 
 -- Open Safe Update Mode
 SET SQL_SAFE_UPDATES = 1;
+
+-- Allow Remote Connection
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
+flush privileges;

c1114831f72ffcc5de50631c756922a5c7e69f24
Mark.Chen(Shao-Yuan Chen) <markchen0114@gmail.com>
2018-04-30 15:06:47 +0800

    Update MySQL.sql

--- MySQL.sql
+++ MySQL.sql
@@ -7,3 +7,37 @@ SET SQL_SAFE_UPDATES = 1;
 -- Allow Remote Connection
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
 flush privileges;
+
+    sudo mysql -u root
+
+    Check your accounts present in your db
+
+    SELECT User,Host FROM mysql.user;
+    +------------------+-----------+
+    | User             | Host      |
+    +------------------+-----------+
+    | admin            | localhost |
+    | debian-sys-maint | localhost |
+    | magento_user     | localhost |
+    | mysql.sys        | localhost |
+    | root             | localhost |
+
+    Delete current root@localhost account
+
+    mysql> DROP USER 'root'@'localhost';
+    Query OK, 0 rows affected (0,00 sec)
+
+    Recreate your user
+
+    mysql> CREATE USER 'root'@'%' IDENTIFIED BY '';
+    Query OK, 0 rows affected (0,00 sec)
+
+    Give permissions to your user (don't forget to flush privileges)
+
+    mysql> GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+    Query OK, 0 rows affected (0,00 sec)
+
+    mysql> FLUSH PRIVILEGES;
+    Query OK, 0 rows affected (0,01 sec)
+
+    Exit MySQL and try to reconnect without sudo.

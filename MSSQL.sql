d4f8880cdd1706e8bb415b9d39263863fdfda890
Mark.Chen(Shao-Yuan Chen) <markchen0114@gmail.com>
2018-11-23 00:10:48 +0800

    Update MSSQL.sql

--- MSSQL.sql
+++ MSSQL.sql
@@ -161,3 +161,21 @@ GO
 ALTER ROLE [db_datareader] ADD MEMBER [HRAPI]
 ALTER ROLE [db_owner] ADD MEMBER [HRERP]
 GO
+
+
+* OPENROWSET configuration
+USE [master]
+GO
+EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0' , N'AllowInProcess' , 1
+GO
+EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0' , N'DynamicParameters' , 1
+GO
+
+
+EXEC sp_configure 'show advanced options', 1 
+GO 
+RECONFIGURE 
+GO 
+EXEC sp_configure 'Ad Hoc Distributed Queries', 1 
+GO 
+RECONFIGURE

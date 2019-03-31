9234789442e4491aeee89a1f1bceadb5acc914f8
Mark.Chen(Shao-Yuan Chen) <markchen0114@gmail.com>
2019-03-31 15:56:01 +0800

    Update MySQL.sql

--- MySQL.sql
+++ MySQL.sql
@@ -4,6 +4,9 @@ SET SQL_SAFE_UPDATES = 0;
 -- Open Safe Update Mode
 SET SQL_SAFE_UPDATES = 1;
 
+--remove password validate
+mysql> uninstall plugin validate_password;
+
 -- Allow Remote Connection
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
 flush privileges;

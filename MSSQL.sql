eea704aa2d5d3f2cfe8393dda6f7ed8290d9075e
Mark.Chen(Shao-Yuan Chen) <markchen0114@gmail.com>
2019-11-28 10:56:33 +0800

    Update MSSQL.sql

--- MSSQL.sql
+++ MSSQL.sql
@@ -179,3 +179,22 @@ GO
 EXEC sp_configure 'Ad Hoc Distributed Queries', 1 
 GO 
 RECONFIGURE
+
+
+-- Check the size of tables in SYSTEM DB
+SELECT sc.name + '.' + t.NAME AS TableName,
+       p.[Rows],
+       ( SUM(a.total_pages) * 8 ) / 1024 AS TotalReservedSpaceMB,
+       ( SUM(a.used_pages) * 8 ) / 1024 AS UsedDataSpaceMB,
+       ( SUM(a.data_pages) * 8 ) / 1024 AS FreeUnusedSpaceMB
+FROM msdb.sys.tables t
+       INNER JOIN msdb.sys.schemas sc ON sc.schema_id = t.schema_id
+       INNER JOIN msdb.sys.indexes i ON t.OBJECT_ID = i.object_id
+       INNER JOIN msdb.sys.partitions p ON i.object_id = p.OBJECT_ID
+                  AND i.index_id = p.index_id
+       INNER JOIN msdb.sys.allocation_units a ON p.partition_id = a.container_id
+WHERE t.type_desc = 'USER_TABLE'
+      AND i.index_id <= 1  --- Heap\ CLUSTERED
+GROUP BY sc.name + '.' + t.NAME,
+      i.[object_id],i.index_id, i.name, p.[Rows]
+ORDER BY ( SUM(a.total_pages) * 8 ) / 1024 DESC
